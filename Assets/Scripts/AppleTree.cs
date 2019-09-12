87c1e3bada825696e7b9c67c0ef2d4534511b3ce
fuzzyliam780 <47114055+fuzzyliam780@users.noreply.github.com>
2019-09-12 01:05:01 -0400

    Catching Apples

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -13,9 +13,19 @@ public class AppleTree : MonoBehaviour{
 
     // Start is called before the first frame update
     void Start(){
-
+        Invoke("DropApple", 2f);
     }
 
+    void DropApple()
+    {                                                  
+
+        GameObject apple = Instantiate<GameObject>(applePrefab);      
+
+        apple.transform.position = transform.position;                  
+
+        Invoke("DropApple", secondsBetweenAppleDrops);                
+
+    }
     // Update is called once per frame
     void Update(){
         Vector3 pos = transform.position;
