61a31ecbbb801905c0942238ff7e8f6666d05526
fuzzyliam780 <47114055+fuzzyliam780@users.noreply.github.com>
2019-09-12 00:42:53 -0400

    Tree Movement

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -2,17 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AppleTree : MonoBehaviour
-{
+public class AppleTree : MonoBehaviour{
+    [Header("Set In Inspector")]
+
+    public GameObject applePrefab;
+    public float speed = 1f;
+    public float leftAndRightEdge = 10f;
+    public float chanceToChangeDirections = 0.1f;
+    public float secondsBetweenAppleDrops = 1f;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start(){
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update(){
+        Vector3 pos = transform.position;
+
+        pos.x += speed * Time.deltaTime;
+
+        transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge){
+
+            speed = Mathf.Abs(speed);
+
+        } else if (pos.x > leftAndRightEdge){
+
+            speed = -Mathf.Abs(speed);
+
+        }
+
+    }
+
+    private void FixedUpdate() {
+        if (Random.value < chanceToChangeDirections)
+        {
+
+            speed *= -1;
+
+        }
     }
 }
